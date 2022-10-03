eb1134a2528aa9f7fce9f3feac0f4d824945d8e7
unknown <em.satyam@gmail.com>
2022-10-03 22:05:51 +0530

    Miner File Changes

--- previous-files/whileloop-16.php
+++ previous-files/whileloop-16.php
@@ -1,16 +1,5 @@
 <?php
-/*
-  $a =1 ;
-  while ($a <= 80){
-      echo $a . ')satyam<br>';
-      $a++;
-  }
-  
-  $a = 70;
-  while ($a >= 40){
-      echo $a . ')satyam<br>';
-      $a--;
-  }*/
+
 
   $a = 70;
   while ($a >= 40){
