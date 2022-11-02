c98ccaf5f0708c4d655c3c4d74e939aec9e7ea25
unknown <em.satyam@gmail.com>
2022-11-02 21:21:06 +0530

    php-integer-file

---
+++ php-variable/integer.php
@@ -0,0 +1,17 @@
+<?php
+// In this page we will write the code of integer variable
+// And also we will see this how it works
+
+// integer
+// It contains the non decimal digits
+// it must have one digit to asign the integer variable
+// it can be positive or negetive
+
+// so lets create a variable
+$a = 19;
+
+echo ($a);
+echo ("<hr />");
+var_dump($a);
+
+
