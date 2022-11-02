e1bdb078d96bdca036d4468859cb56344a947240
unknown <em.satyam@gmail.com>
2022-11-02 21:20:39 +0530

    php-float-file

---
+++ php-variable/float.php
@@ -0,0 +1,12 @@
+<?php
+// In this page we will learn about the float variabe 
+
+// Float Data-type 
+//  The float contains the decimal numbers inside  itself to asign the variable 
+
+// let's create a variable 
+$a = 5494.544;
+echo ($a);
+echo ("<hr />");
+
+var_dump($a);
