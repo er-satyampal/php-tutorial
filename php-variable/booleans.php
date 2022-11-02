802b58dd0ec4900ef8415175257019cb14aa9cb4
unknown <em.satyam@gmail.com>
2022-11-02 21:20:20 +0530

    php-booleans-file

---
+++ php-variable/booleans.php
@@ -0,0 +1,24 @@
+<?php
+// Booleans 
+// It represents the true and false the variable 
+// To assign the the boolean variable we make it conditional. 
+ 
+// lets create a variable. 
+
+$a =  45;
+
+// there we will create a condition that will determines the value 
+
+echo ($a == 23 );
+
+echo ("<hr />");
+
+echo ($a !== 23);
+ 
+echo ("<hr />");
+
+var_dump($a == 23 );
+
+echo ("<hr />");
+
+var_dump($a !== 23);
