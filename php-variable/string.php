461a110f5d0bdf190b14189aa44abf190672b279
unknown <em.satyam@gmail.com>
2022-11-02 21:21:31 +0530

    php-string-file

---
+++ php-variable/string.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = "Satyam";
+
+echo ($a);
+echo ("<hr />");
+var_dump($a);
+
+
+echo ("<hr />");
+
+// if we wants to see the integer value inside like as the string value
+// then we can easily do this 
+// but this integer will be treat as the string
+// such as this
+$x = "454";
+
+echo ($x);
+echo ("<hr />");
+var_dump($x);
