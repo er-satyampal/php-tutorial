ba2cafd867a3264b25305cdc951e6637f5e52009
unknown <em.satyam@gmail.com>
2022-11-07 14:37:58 +0530

    Switch-Case in PHP

---
+++ Lrn-progress/switch.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$ac = "Shiv";
+switch ($ac) {
+    
+    case 'Monu':
+        echo "Monu name is executing";
+        break;
+        
+    case 'Shya,':
+        echo "Monu name is executing";
+        break;
+        
+    case 'Dev':
+        echo "Monu name is executing";
+        break;
+
+    case 'Suman':
+        echo "Monu name is executing";
+        break;
+
+    default:
+        echo "No case met";
+        break;
+}
