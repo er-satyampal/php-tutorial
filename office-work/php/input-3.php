ee006efadf9a62f7622ce85c429f37d085b030d2
unknown <em.satyam@gmail.com>
2022-11-22 05:16:04 +0530

    Given Values Form Created

--- office-work/php/input-3.php
+++ office-work/php/input-3.php
@@ -1,11 +1,23 @@
 <?php
 
 
+// Given Values Appearing at here ----------End/
+
 echo"Product Cost :- ".($_GET["p_cost"])."<br/>";
 
+echo"Tax :- ".($_GET["tax"])."<br/>";
+
+echo "Discount :- ".($_GET["radio_button"])."<br/>";
+
+echo"<hr/>";
+
+// Given Values Appearing at here ----------End/
 
 
-$a = ($_REQUEST['radio_button']);
+
+$
+
+$discount = ($_REQUEST['radio_button']);
 
 
 echo($a);
