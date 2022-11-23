d25bcd5eb919bbc1f7ac364495adca281fa3ad94
unknown <em.satyam@gmail.com>
2022-11-23 13:34:24 +0530

    Minor Issues

--- office-work/php/input-3.php
+++ office-work/php/input-3.php
@@ -4,6 +4,7 @@ echo"Product Cost :- ".($_GET["p_cost"])."₹"."<br />";
 echo"Tax :- ".($_GET["tax"])."%"."<br />";
 echo "Discount :- ".($_GET["radio_button"])."%"."<br />";
 echo"<hr/>";
+
 // // Given Values Appearing at here ----------End/
 // // Variables are atarting from here-----------------+
 // $product_cost = ($_GET["p_cost"]);
