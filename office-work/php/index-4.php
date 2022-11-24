3fdabe478655a34e32a16360df6b371585f8cbc4
unknown <em.satyam@gmail.com>
2022-11-24 22:34:06 +0530

    Data Modified on 24

---
+++ office-work/php/index-4.php
@@ -0,0 +1,14 @@
+<?php
+
+echo"First Name :- ".($_GET["fname"])."<br />";
+
+echo"Last Name :- ".($_GET["lname"])."<br />";
+
+echo"Your Age :- ".($_GET["dob"])."<br />";
+
+echo"Your Email I'd :- ".($_GET["e_mail"])."<br />";
+
+echo"Your Phone Number :- ".($_GET["number"])."<br />";
+
+echo"Your Country :- ".($_GET["country"])."<br />";
+?>
