7ee68bcfd6d648100de87478e1b07883d14ae947
unknown <em.satyam@gmail.com>
2022-12-19 23:46:55 +0530

    Submit file Modified #191022

--- office-work/database/submit.php
+++ office-work/database/submit.php
@@ -9,7 +9,10 @@ $country = $_GET["country"];
 
 require_once("db/connection.php");
 
-$sth = $connection->prepare("INSERT INTO `students` (f_name, l_name, dob, e_mail, number, country) VALUES (:db_first_name, :db_last_name, :db_dob, :db_email, :db_mobile_no, :db_country)"); 
+$sth = $connection->prepare("INSERT INTO `students`
+ (f_name, l_name, dob, e_mail, number, country) VALUES 
+ (:db_first_name, :db_last_name, :db_dob, :db_email,
+  :db_mobile_no, :db_country)"); 
 
 $sth->bindParam(':db_first_name', $first_name);
 $sth->bindParam(':db_last_name', $last_name);
@@ -18,7 +21,6 @@ $sth->bindParam(':db_email', $email);
 $sth->bindParam(':db_mobile_no', $mobile_no);
 $sth->bindParam(':db_country', $country);
 	
-
 if($sth->execute()){
 echo "Data Inserted Successfuly";	
 }else{
