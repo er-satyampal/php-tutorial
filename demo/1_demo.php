31d82b7c821fecca6a83654edb9255608aa25e14
unknown <em.satyam@gmail.com>
2023-01-24 19:08:29 +0530

    Minor File Chnages #240123

---
+++ demo/1_demo.php
@@ -0,0 +1,29 @@
+<?php
+$food = 5;
+switch ($food){
+case 1:
+    echo "wow that is roll";
+     break;
+    case 2:
+        echo "that is samose";
+        break;
+        case 3:
+            echo "that is egg";
+            break;
+            case 4:
+                echo "that is omlette";break;
+                case 5:
+                    echo "wow so nice "; break;
+                default:
+                echo "kutte satyam";
+                break;
+
+
+
+
+
+
+
+
+
+}
